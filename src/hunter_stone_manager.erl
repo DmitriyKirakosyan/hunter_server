2a64f71bbaf57a6d12833aa914cefdbad41200e4
Dima <dmitriy.kirakosyan@gmail.com>
2014-08-21 15:47:24 +0400

    generate stones

--- src/hunter_stone_manager.erl
+++ src/hunter_stone_manager.erl
@@ -2,8 +2,8 @@
 
 -export ([create_stones/0]).
 
--define (MAP_WIDTH, 1024).
--define (MAP_HEIGHT, 768).
+-define (MAP_WIDTH, 1350).
+-define (MAP_HEIGHT, 1350).
 
 -define (FIELDS_NUM, 5).
 
@@ -11,4 +11,15 @@
 
 
 create_stones() ->
-    ok.
+    [get_random_stone(I) || I <- lists:seq(0, ?FIELDS_NUM * ?FIELDS_NUM - 1)].
+
+get_random_stone(Position) ->
+    FieldWidth = ?MAP_WIDTH / ?FIELDS_NUM,
+    FieldHeight = ?MAP_HEIGHT / ?FIELDS_NUM,
+
+    XOffset = round( (Position rem ?FIELDS_NUM) * FieldWidth ),
+    YOffset = round( (Position div ?FIELDS_NUM) * FieldHeight ),
+
+    X = XOffset + round( random:uniform(round(FieldWidth/2)) + FieldWidth/4 ),
+    Y = YOffset + round( random:uniform(round(FieldHeight/2)) + FieldHeight/4 ),
+    {X, Y}.
